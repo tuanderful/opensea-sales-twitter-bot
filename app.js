7ac95fe61b86495911b9fe639f7fffb96d6c5956
Tuan Huynh <tuanlh@gmail.com>
2022-04-23 12:47:41 -0700

    Fix timer for last transfer

--- app.js
+++ app.js
@@ -100,11 +100,11 @@ setInterval(() => {
 
 
 setTimeout(() => {
-  const lastTransferTime = cache.get('lastTransferTime', null) || moment().startOf('minute').subtract(89, "seconds").unix();
-
-  console.log(`Last transfer (in seconds since Unix epoch): ${cache.get('lastTransferTime', null)}`);
 
   setInterval(() => {
+    const lastTransferTime = cache.get('lastTransferTime', null) || moment().startOf('minute').subtract(89, "seconds").unix();
+
+    console.log(`Last transfer (in seconds since Unix epoch): ${cache.get('lastTransferTime', null)}`);
     axios.get('https://api.opensea.io/api/v1/events', {
       headers: {
         'X-API-KEY': process.env.X_API_KEY
